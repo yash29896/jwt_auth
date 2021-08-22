f7879cd771646453cd09d1814406f394aa5652ec
yash rk <ya17kun@gmail.com>
2021-08-22 21:56:00 +0530

    abc

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const User = require("./models/user");
 const mongoose = require("mongoose");
 const path = require("path");
 const bcrypt = require("bcrypt");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 mongoose.connect("mongodb://localhost:27017/yash", {
   useNewUrlParser: true,
@@ -18,16 +20,44 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname + "/views"));
-app.use(express.urlencoded({ extended: true }));
+const verifyToken = async (req, res, next) => {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    const bearerToken = bearerHeader.split(" ")[1];
+    req.token = bearer;
+    return next();
+  } else {
+    res.redirect("/login");
+  }
+};
 
+//app.set("view engine", "ejs");
+//app.set("views", path.join(__dirname + "/views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+  
 app.get("/login", (req, res) => {
   res.render("login");
 });
 
-app.post("/login", (req, res) => {
-  res.json(req.body);
+app.post("/login", async (req, res) => {
+  const usr = await User.findOne({ emailid: req.body.user.emailid });
+  const isAuth = await bcrypt.compare(req.body.user.password, usr.password);
+  if (isAuth) {
+    jwt.sign({ usr }, process.env.ACCESS_TOKEN_SECRET, (err, token) => {
+      res.json({ token });
+    });
+  }
+});
+
+app.get("/details", verifyToken, async (req, res) => {
+  jwt.verify(req.token, process.env.ACCESS_TOKEN_SECRET, (err, userData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.send(userData);
+    }
+  });
 });
 
 app.get("/signup", (req, res) => {
